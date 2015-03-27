028993d970a218aa0b1397c7afb0f5f70a1480a7
Masaki Kagaya <masakielastic@gmail.com>
2015-03-27 19:32:57 +0900

    updated index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 include '/path/to/wp-blog-header.php';
+$js_url = $request->getUri().'js/example.js';
 
 add_action('wp_head', function() use($app) {
 	echo '<meta name="csrf-token" content="'. $app['session']->get('csrf-token') .'" />';
 });
-add_action( 'wp_enqueue_scripts', function() use($request) {
+add_action( 'wp_enqueue_scripts', function() use($js_url) {
     wp_enqueue_script(
         'example.js',
-        'https://example.org/path/to/web/js/example.js', [], null, true);
+        $js_url, [], null, true);
 });
 
 get_header();
@@ -18,8 +19,6 @@ get_header();
     <div id="content" class="site-content" role="main">
       <article id="post-1" class="post-1 post type-post status-publish format-standard hentry category-1">
         <div class="entry-content">
-          <h2>もみねこ堂カード決済</h2>
-
           <form id="paymentForm" action="#" method="post">
             <div>金額(円)</div>
             <input type="text" id="inputAmount" placeholder="金額を入力">
@@ -41,7 +40,7 @@ get_header();
 
 <script>
 // WebPay.testMode = true;
-var payment_uri = "<?php echo $app['config']['payment_uri']; ?>";
+var charge_uri = "<?php echo $app['config']['charge_uri']; ?>";
 </script>
 
 <?php
